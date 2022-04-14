322795336afb1cc0455d9b3f934ee3346e88ba07
zhixin.zheng <zhixzheng0813@gmail.com>
2022-04-14 11:34:49 -0400

    longest substring with at most k distinct elements [sliding window]

--- Slide_window/lc-340.py
+++ Slide_window/lc-340.py
@@ -8,28 +8,35 @@ Explanation: The substring is "aabbb" which its length is 5.
 from typing import List
 
 class Solution:
-    def lengthOfLongestSubstringTwoDistinct(self, s: str) -> int:
+    def lengthOfLongestSubstringKDistinct(self, s: str, k: int) -> int:
         N = len(s)
         l = 0
         max_length = 0
         dict = {}
-        cur = []
+        cur = set() # 也可以用[]，list 也可以指定元素抛出， list 效率越高于set, 区别就是一个是add 一个是append
         for r in range(N):
             # if s[r] not in the original list
             if (s[r] not in dict.keys() or dict[s[r]] < l):
-                cur.append(s[r])
+                cur.add(s[r])
                 dict[s[r]] = r
-                while (len(cur) > 2 and l <= r):
+                while (len(cur) > k and l <= r):
                     if (dict[s[l]] == l):
                         cur.remove(s[l])
-                    l += 1  # 这里只能一个个遍历，不像第三题可以直接取 l 和 map中次元素最大的index+1
+                    l += 1       
             elif (dict[s[r]] >= l):  # if s[r] in the original list
                 dict[s[r]] = r
             max_length = max(max_length, r-l+1)
+
         return max_length
 
-s = "ababcbcbaaabbdef"
-lc_159 = Solution()
-print(f'lc 159 solution: {lc_159.lengthOfLongestSubstringTwoDistinct(s)}')
+
+s = "a@b$5!a8alskj234jasdf*()@$&%&#FJAvjjdaurNNMa8ASDF-0321jf?>{}L:fh"
+
+k = 10
+
+
+
+lc_340 = Solution()
+print(f'lc 340 solution: {lc_340.lengthOfLongestSubstringKDistinct(s,k)}')
 
             
