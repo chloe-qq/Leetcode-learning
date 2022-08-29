d0f871a6800df53e77ffdf48ca2a3d2ede695147
zhixin.zheng <zhixzheng0813@gmail.com>
2022-08-29 15:14:58 -0400

    [Greedy]

--- Greedy/lc-45.py
+++ Greedy/lc-45.py
@@ -1,20 +1,24 @@
 """
 simplified BFS
-
+没走一步 会有一个range [cur + 0, cur + nums[cur]]可走
+走哪一步呢？ 取决于[cur + 0, cur + nums[cur]]每一个点可走的步数
+所以要取上述最大值farthest（遍历） 当走到cur + nums[cur]时候，更新最大值, 且 step + 1
 """
 from typing import List
 
+
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        step = 0
-        left = 0
-        right = 0
-        while (right < len(nums)-1):
-            farthest = 0
-            for i in range(left,right+1):
-                farthest = max(farthest,i+nums[i])
-            left = right + 1
-            right = farthest
-            step += 1
-        return step
-    
+        steps = 0
+        cur_jump_end = 0
+        farthest = 0
+        N = len(nums)
+        for i in range(N-1):
+            farthest = max(farthest, i + nums[i])
+            # next move will fall somewhere between [start : end] 
+            # to find the minimum number of jumps to reach the end of the array, we must determine which place will take us the farthest in the next jump.
+            if (i == cur_jump_end):
+                steps += 1
+                cur_jump_end  = farthest
+        return steps
+                
--- Greedy/lc-55.py
+++ Greedy/lc-55.py
@@ -12,13 +12,27 @@ Explanation: You will always arrive at index 3 no matter what. Its maximum jump 
 from typing import List
 
 class Solution:
-    def canJump(self, nums: List[int]) -> bool:
+    def canJump_method1(self, nums: List[int]) -> bool:
         goal = len(nums) -1
         for i in range(len(nums)-1, -1, -1):
             if (i + nums[i] >= goal):
                 goal = i
                 
         return True if goal == 0 else False
+    
+    def canJump(self, nums: List[int])-> bool:
+        # only need to check whether farthest will cover N = len(nums)-1
+        final = len(nums)-1
+        i = 0
+        farthest = 0
+        while (farthest < final and i < final):
+            farthest = max(farthest, i + nums[i])
+            if (farthest <= i):
+                return False
+            i += 1
+        return True if farthest >= final else False
+            
+            
 nums = [3,2,1,0,4]
 lc_55 = Solution()
 print(f'{lc_55.canJump(nums)}')
