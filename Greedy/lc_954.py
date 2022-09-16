4790d94558cbf45f18915f2e93256bc18562b6ae
zhixin.zheng <zhixzheng0813@gmail.com>
2022-09-16 15:36:21 -0400

    [hashmap]

--- Greedy/lc_954.py
+++ Greedy/lc_954.py
@@ -1,6 +1,26 @@
 # very much like lc_2007
 import collections
 from typing import List
+
+
+# optimization
+class Solution:
+    def canReorderDoubled(self, arr: List[int]) -> bool:
+        frequency = collections.Counter(arr)
+        
+        arr = sorted(arr, key = abs)
+
+        for x in arr:
+            if (frequency[x] == 0): 
+                continue
+            if (frequency[2*x] == 0):
+                return False
+            frequency[2*x] -= frequency[x]
+            if (frequency[2*x]<0):
+                return False
+            frequency[x] = 0
+        return True
+
 class Solution:
     def canReorderDoubled(self, arr: List[int]) -> bool:
         frequency = collections.Counter(arr)
